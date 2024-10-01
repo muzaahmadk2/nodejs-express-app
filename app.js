e0602bb00803072e52d51461eec1eccb2551cdff
Muzammil Ahmad K <Muzammil.Ahmad.K@ibm.com>
2024-10-01 16:29:04 +0530

    adding a request driven login solution and setting a dummy cookie example

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const User = require("./models/user");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 // db.execute("SELECT * FROM products")
 //   .then((res) => {
@@ -38,6 +39,7 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
