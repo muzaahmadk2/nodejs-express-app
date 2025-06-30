3e59c0a8c59c0672604f499a3402329d0fea3c52
Muzammil Ahmad K <muzaahmadk2@gmail.com>
2025-06-30 17:34:42 +0530

    downloading a file

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+
 const Order = require("../models/order");
 const Product = require("../models/product");
 
@@ -112,3 +115,18 @@ exports.getOrders = (req, res, next) => {
 //     pageTitle: "Checkout",
 //   });
 // };
+
+exports.getInvoice = (req, res, next) => {
+  const orderId = req.params.orderId;
+  const invoiceName = "invoice-" + orderId + ".pdf";
+  const invoicePath = path.join("data", "invoices", invoiceName);
+
+  fs.readFile(invoicePath, (err, data) => {
+    if (err) {
+      return next(err);
+    }
+    res.setHeader("content-type", "application/pdf");
+    res.setHeader("content-disposition", "attachment; filename=" + invoiceName);
+    res.send(data);
+  });
+};
